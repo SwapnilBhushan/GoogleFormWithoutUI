aefeaa2f837fcfe3dc06075bb07934e8d8915fc7
Swapnil Bhushan <swapnilbhushan2020@gmail.com>
2024-01-25 17:50:03 +0530

    updated

---
+++ react/src/store/formSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { FormDetail } from '../AppStore.type';
+
+interface FormState {
+  forms: FormDetail[];
+}
+
+const initialState: FormState = {
+  forms: [],
+};
+
+const formSlice = createSlice({
+  name: 'form',
+  initialState,
+  reducers: {
+    setForms: (state, action: PayloadAction<FormDetail[]>) => {
+      state.forms = action.payload;
+    },
+  },
+});
+
+export const { setForms } = formSlice.actions;
+export default formSlice.reducer;
